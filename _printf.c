2ca556df6088a9cb61a8e6b08e76aa7d8d25ca50
amir <lhm76726@gmail.com>
2023-09-16 22:34:31 +0100

    LEET

--- _printf.c
+++ _printf.c
@@ -1,43 +1,94 @@
+#include <stdarg.h>
+#include <unistd.h>
 #include "main.h"
+
 /**
- * _printf - printf function
- * @format: const char pointer
- * Return: b_len
+ * _printf - Custom printf function
+ * @format: Format string
+ * Return: Number of characters printed (excluding null byte)
  */
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0};
+    va_list args;
+    int printed_chars = 0;
 
-	register int count = 0;
+    va_start(args, format);
 
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p; p++)
-	{
-		if (*p == '%')
-		{
-			p++;
-			if (*p == '%')
-			{
-				count += _putchar('%');
-				continue;
-			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
-	}
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == '\0')
+                break;
+            if (*format == 'c')
+                printed_chars += print_char(args);
+            else if (*format == 's')
+                printed_chars += print_string(args);
+            else if (*format == '%')
+                printed_chars += print_percent();
+            else
+            {
+                write(1, &(*format), 1);
+                printed_chars++;
+            }
+        }
+        else
+        {
+            write(1, &(*format), 1);
+            printed_chars++;
+        }
+        format++;
+    }
+
+    va_end(args);
+
+    return (printed_chars);
 }
+
+/**
+ * print_char - Print a character
+ * @args: va_list containing the character to print
+ * Return: Number of characters printed
+ */
+int print_char(va_list args)
+{
+    char c = va_arg(args, int);
+
+    write(1, &c, 1);
+    return (1);
+}
+
+/**
+ * print_string - Print a string
+ * @args: va_list containing the string to print
+ * Return: Number of characters printed
+ */
+int print_string(va_list args)
+{
+    char *str = va_arg(args, char *);
+    int printed_chars = 0;
+
+    if (!str)
+        str = "(null)";
+
+    while (*str)
+    {
+        write(1, str, 1);
+        printed_chars++;
+        str++;
+    }
+
+    return (printed_chars);
+}
+
+/**
+ * print_percent - Print a percent sign
+ * Return: Number of characters printed
+ */
+int print_percent(void)
+{
+    write(1, "%", 1);
+    return (1);
+}
+
