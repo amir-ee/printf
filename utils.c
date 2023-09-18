c959990e224e32a4976fab88900cca7d5a5fcfbc
amir <lhm76726@gmail.com>
2023-09-18 11:56:10 +0100

    im_pointing_to_z

--- utils.c
+++ utils.c
@@ -1,17 +1,11 @@
 #include "main.h"
 
 /**
- * is_printable - Checks if a character is printable
- * @c: The character to be checked
+ * is_printable - Evaluates if a char is printable
+ * @c: Char to be evaluated.
  *
- * Description:
- * This function checks whether a character falls within the printable ASCII
- * range (from 32 to 126) and returns 1 if it is printable, otherwise 0.
- *
- * Return:
- * 1 if the character is printable, 0 otherwise.
+ * Return: 1 if c is printable, 0 otherwise
  */
-
 int is_printable(char c)
 {
 	if (c >= 32 && c < 127)
@@ -21,24 +15,16 @@ int is_printable(char c)
 }
 
 /**
- * append_hexa_code - Appends the hexadecimal representation of a character to a buffer
- * @ascii_code: The ASCII code of the character to be converted
- * @buffer: The buffer to which the hexadecimal representation is appended
- * @i: The current index in the buffer
- *
- * Description:
- * This function appends the hexadecimal representation of the given ASCII code
- * to the buffer at the specified index 'i'. It returns the number of characters
- * (3) appended to the buffer.
- *
- * Return:
- * The number of characters (3) appended to the buffer.
+ * append_hexa_code - Append ascci in hexadecimal code to buffer
+ * @buffer: Array of chars.
+ * @i: Index at which to start appending.
+ * @ascii_code: ASSCI CODE.
+ * Return: Always 3
  */
-
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
-
+	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
@@ -52,17 +38,11 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 }
 
 /**
- * is_digit - Checks if a character is a digit
- * @c: The character to be checked
+ * is_digit - Verifies if a char is a digit
+ * @c: Char to be evaluated
  *
- * Description:
- * This function checks whether a character is a digit (0-9) and returns 1
- * if it is a digit, otherwise 0.
- *
- * Return:
- * 1 if the character is a digit, 0 otherwise.
+ * Return: 1 if c is a digit, 0 otherwise
  */
-
 int is_digit(char c)
 {
 	if (c >= '0' && c <= '9')
@@ -72,18 +52,12 @@ int is_digit(char c)
 }
 
 /**
- * convert_size_number - Converts a number to a specific size
- * @num: The number to be converted
- * @size: The desired size (S_LONG or S_SHORT)
+ * convert_size_number - Casts a number to the specified size
+ * @num: Number to be casted.
+ * @size: Number indicating the type to be casted.
  *
- * Description:
- * This function converts a given number to a specific size (long int or short int)
- * based on the 'size' parameter and returns the converted number.
- *
- * Return:
- * The converted number with the specified size.
+ * Return: Casted value of num
  */
-
 long int convert_size_number(long int num, int size)
 {
 	if (size == S_LONG)
@@ -95,18 +69,12 @@ long int convert_size_number(long int num, int size)
 }
 
 /**
- * convert_size_unsgnd - Converts an unsigned number to a specific size
- * @num: The unsigned number to be converted
- * @size: The desired size (S_LONG or S_SHORT)
+ * convert_size_unsgnd - Casts a number to the specified size
+ * @num: Number to be casted
+ * @size: Number indicating the type to be casted
  *
- * Description:
- * This function converts a given unsigned number to a specific size (unsigned long int
- * or unsigned short int) based on the 'size' parameter and returns the converted number.
- *
- * Return:
- * The converted unsigned number with the specified size.
+ * Return: Casted value of num
  */
-
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
 	if (size == S_LONG)
